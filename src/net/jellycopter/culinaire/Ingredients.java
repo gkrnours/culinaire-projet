f027206d8002308a27d351194d32e6de1fd76b32
ludovic <couesl@gmail.com>
2011-12-08 09:41:06 +0100

    Ajout de m?thodes pour emballer/d?baller Recettes et Ingredients

--- src/net/jellycopter/culinaire/Ingredients.java
+++ src/net/jellycopter/culinaire/Ingredients.java
@@ -6,7 +6,31 @@ public abstract class Ingredients {
 
 String nom;
 
-HashSet hs = new HashSet();
+static Map<String, Ingredients> hs = new HashMap<String, Ingredients>();
+static public void ajouter(Ingredients i){
+	hs.put(i.getNom(), i);
+}
+static public Ingredients get(String nom){
+	return hs.get(nom);
+}
+static public Ingredients[] getAll(){
+	return hs.values().toArray(new Ingredients[hs.size()]);
+}
+
+public static String[] emballer(Ingredients i){
+	return new String[]{ i.getNom(), i.getClass().toString() };
+}
+public static Ingredients deballer(String[] s){
+	Ingredients i;
+	if(s[1].equals("Sucre")){
+		i = new Sucre(s[0]);
+	}else if(s[1].equals("Sale")){
+		i = new Sale(s[0]);
+	}else{
+		i = new Basique(s[0]);
+	}
+	return i;
+}
 
 public Ingredients(String nom) {
 
@@ -14,6 +38,7 @@ super();
 
 this.nom = nom;
 
+hs.put(nom, this);
 }
 
 public String getNom() {
--- src/net/jellycopter/culinaire/Recettes.java
+++ src/net/jellycopter/culinaire/Recettes.java
@@ -1,20 +1,63 @@
 package net.jellycopter.culinaire;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public abstract class Recettes {
+	static Map<String, Recettes> hs = new HashMap<String, Recettes>();
 
 	String nom ;
 	int temps;
 	Map<Ingredients , Integer> ingredients = new HashMap<Ingredients, Integer>();
 	
+	public String[] emballer(){
+		String[] e = new String[4];
+		e[0] = nom;
+		e[1] = temps+"";
+		StringBuilder i = new StringBuilder();
+		for(Map.Entry<Ingredients, Integer> elem: ingredients.entrySet() ){
+			i
+			.append(elem.getKey().getNom()).append(":")
+			.append(elem.getKey().getClass().toString().split("[.]")[3])
+			.append(":")
+			.append(elem.getValue()).append(";");
+		}
+		e[2] = i.toString();
+		e[3] = getClass().toString().split("[.]")[3];
+		// la classe est le 4ème champ lors du split
+		System.out.println(e[3]);
+		return e;
+	}
+	public static Recettes deballer(String[] e){
+		Map<Ingredients, Integer> ing = new HashMap<Ingredients, Integer>();
+		for(String s: e[2].split(";")){
+			String[] raw_ing = s.split(":");
+			ing.put(Ingredients.deballer(raw_ing),
+						Integer.parseInt(raw_ing[2]));
+		}
+		Recettes r;
+		if(e[3] == "plaqueChaufante"){
+			r = new plaqueChaufante(e[0], Integer.parseInt(e[1]), ing);
+		}else if(e[3] == "MicroOndes"){
+			r = new MicroOndes(e[0], Integer.parseInt(e[1]), ing);
+		}else if(e[3] == "Four"){
+			r = new Four(e[0], Integer.parseInt(e[1]), ing);
+		}else{
+			r = new Froid(e[0], Integer.parseInt(e[1]), ing);
+		}
+		return r;
+	}
+	public static Recettes[] getAll(){
+		return hs.values().toArray(new Recettes[hs.size()]);
+	}
 	
 	public Recettes(String nom, int temps, Map<Ingredients, Integer> ingredients) {
 		super();
 		this.nom = nom;
 		this.temps = temps;
 		this.ingredients = ingredients;
+		hs.put(nom, this);
 	}
 
 
